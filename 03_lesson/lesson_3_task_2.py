52f7885a097901f25a78531e4461b4dffe7db424
Анастасия <nastia.klimenkova93@yahoo.com>
2025-07-30 11:25:02 +0600

    Third commit

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -5,7 +5,10 @@ catalog = [
         Smartphone(brand="Samsung", model="A5", number="+79876545676"),
         Smartphone(brand="Apple", model="13", number="+79876545676"),
         Smartphone(brand="Xiaomi", model="S2", number="+79875674554"),
+        Smartphone(brand="Honor", model="B5", number="+76787654567"),
+        Smartphone(brand="Poco", model="Q1", number="+78766789098")
         ]
 
 for Smartphone in catalog:
-    print(f"{Smartphone.brand} - {Smartphone.model} - {Smartphone.number} ")
+    print(f"{Smartphone.brand} - {Smartphone.model}. {Smartphone.number} ")
+
